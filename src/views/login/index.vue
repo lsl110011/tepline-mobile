6d14a4e8c40726930148c6462b88b4bfa5b3f128
阿力 <1924185420@qq.com>
2019-07-10 18:06:32 +0800

    '登录loading加载和表单验证'

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -13,12 +13,14 @@
           clearable
           label="手机号"
           placeholder="请输入手机号"
+          :error-message="errors.mobile"
         />
         <van-field
           v-model="user.code"
           type="password"
           label="密码"
           placeholder="请输入密码"
+          :error-message="errors.code"
           required
         />
       </van-cell-group>
@@ -26,6 +28,8 @@
          <van-button
          class="login-btn"
           type="info"
+          :loading="loginLoading"
+          loading-text="加载中..."
           @click.prevent="handleLogin"
           >登录</van-button>
        </div>
@@ -35,7 +39,6 @@
 
 <script>
 import { login } from '@/api/user'
-// import { constants } from 'crypto'
 export default {
   name: 'LoginIndex',
   data () {
@@ -43,12 +46,38 @@ export default {
       user: {
         mobile: '15036015854',
         code: '123456'
+      },
+      loginLoading: false, // 控制登录按钮的loading状态
+      errors: {
+        mobile: '',
+        code: ''
       }
     }
   },
   methods: {
     async handleLogin () {
       try {
+        // 发送请求之前，校验表单数据，校验通过，才进行登录
+        const { mobile, code } = this.user
+        const errors = this.errors
+
+        if (mobile.length) {
+          errors.mobile = ''
+        } else {
+          errors.mobile = '手机号不能为空'
+          return
+        }
+
+        if (code.length) {
+          errors.code = ''
+        } else {
+          errors.code = '验证码不能为空'
+          return
+        }
+
+        // 表单验证通过，发送请求，loading加载
+        this.loginLoading = true
+
         const data = await login(this.user)
         // 登录成功以后，提交moutation完成状态的修改
         this.$store.commit('setUser', data)
@@ -61,6 +90,7 @@ export default {
         console.log(err)
         console.log('登录失败')
       }
+      this.loginLoading = false
     }
   }
 }
