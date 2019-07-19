e82446d7c28285241d07cbab7d44a85405e30094
阿力 <1924185420@qq.com>
2019-07-19 21:49:28 +0800

    '搜索页面布局'

---
+++ src/views/search/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <van-search
+     placeholder="请输入搜索关键词" v-model="serchText"
+     show-action />
+     <!-- 联想建议列表 -->
+     <van-cell-group>
+        <van-cell title="单元格" icon="search" />
+        <van-cell title="单元格" icon="search"  />
+    </van-cell-group>
+     <!-- 历史记录 -->
+     <van-cell
+       title="历史记录"
+     >
+     <van-icon
+       slot="right-icon"
+       name="delete"
+       style="line-height: inherit;"
+     />
+     </van-cell>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SearchIndex',
+  data () {
+    return {
+      serchText: ''
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
